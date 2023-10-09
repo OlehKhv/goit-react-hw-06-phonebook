9a5517f8fc8abcfca3fedb0ae3a4db5a911c1b8d
OlehKhv <126176715+OlehKhv@users.noreply.github.com>
2023-10-09 11:00:44 +0300

    fix persit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,13 @@
-import { persistStore, persistReducer } from 'redux-persist';
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { reducer } from './reducer';
 
@@ -14,6 +23,20 @@ const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
     reducer: persistedReducer,
+    middleware(getDefaultMiddleware) {
+        return getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER,
+                ],
+            },
+        });
+    },
 });
 
 export const persistor = persistStore(store);
